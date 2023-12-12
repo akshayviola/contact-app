908f2b9c34c59f713be7842de303d375a1835436
akshayviola <akshaysunil201@gmail.com>
2023-12-12 20:06:28 +0530

    contact-app

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AllContactComponent } from './all-contact/all-contact.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+
+const routes: Routes = [
+  { path: '', component: AllContactComponent },
+  { path: 'add-contact', component: AddContactComponent },
+  { path: 'view-contact/:id', component: ViewContactComponent },
+  { path: 'edit-contact/:id', component: EditContactComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AllContactComponent } from './all-contact/all-contact.component';
+import { AddContactComponent } from './add-contact/add-contact.component';
+import { ViewContactComponent } from './view-contact/view-contact.component';
+import { EditContactComponent } from './edit-contact/edit-contact.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipe/filter.pipe';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AllContactComponent,
+    AddContactComponent,
+    ViewContactComponent,
+    EditContactComponent,
+    FilterPipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
